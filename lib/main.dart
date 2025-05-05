a50cf0325b539aa46802b22c14a44055d3f4ff1c
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-05-05 15:01:19 -0300

    teste inicial do banco de dados

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+import 'package:app_zapzap/Home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Necessário para usar Firebase antes de runApp
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Firestore atual usa FirebaseFirestore.instance
+  await FirebaseFirestore.instance.collection("usuarios").doc("001").set({
+    "Nome": "Mauricio",
+  });
+
+  runApp(MaterialApp(home: Home()));
+}
